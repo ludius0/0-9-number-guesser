bc34ed264bf6a0ab440db7748917d82662f83aba
ludius0 <playerofzero@icloud.com>
2020-07-11 01:04:55 +0200

    Have functional loading dataset; add plotting charts and training epochs

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
-import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 class MnistDataset(Dataset):
     def __init__(self, path):
         # CSV reader
-        self.data_df = pd.read_csv(path, header=None)
-
-    def plot(self, index):
-        arr = self.data_df.iloc[index, 1:].values.reshape(28, 28)
-        plt.title("label = " + str(self.data_df.iloc[index, 0]))
-        plt.imshow(arr, interpolation="none", cmap="Blues")
+        training_data_file = open("mnist_dataset/mnist_train.csv", 'r')
+        self.data_df = training_data_file.readlines()
+        training_data_file.close()
 
     def __len__(self):
-        return self.data_df
+        return len(self.data_df)
 
     def __getitem__(self, index):
-        label = self.data_df.iloc[index, 0]
+        img = list(map(int, self.data_df[index].split(',')))
+        label = img[0]
         target = torch.zeros((10))
         target[label] = 1.0
-        img_values = torch.FloatTensor(self.data_df.iloc[index, 1:].values) / 255.0
+        img_values = torch.FloatTensor(img[1:]) / 255.0
 
         return label, img_values, target
+
+    def plot(self, index):
+        img = list(map(int, self.data_df[index].split(',')))
+        arr = np.array(img[1:]).reshape(28, 28)
+        plt.title("label = " + str(img[0]))
+        plt.imshow(arr, interpolation="none", cmap="Blues")
+        plt.show()
+
+
 
 class DigitClassifier(nn.Module):
     def __init__(self):
@@ -38,6 +45,10 @@ class DigitClassifier(nn.Module):
         self.loss_function = nn.MSELoss()
         self.optimiser = torch.optim.SGD(self.parameters(), lr=0.01)
 
+        # Timestap of loss for ploting progress
+        self.progress = []
+        self.counter = 0
+
     def forward(self, inputs):
         return self.model(inputs)
 
@@ -47,5 +58,44 @@ class DigitClassifier(nn.Module):
         loss = self.loss_function(outputs, targets)
 
         self.optimiser.zero_grad()
-        loss.backwards()
+        loss.backward()
         self.optimiser.step()
+
+        self.counter += 1
+        self.progress.append(loss.item())
+        
+        if self.counter % 10000 == 0:
+            print(f"Already trained on {self.counter} images.")
+    
+    def plot_progress(self):
+        x_size = [i for i in range(self.counter)]
+
+        # Plot
+        fig, ax = plt.subplots(figsize=(16, 8))
+        ax.set_title("Loss over time (x=trained images, y=loss values)")
+        ax.plot(x_size, self.progress)
+        plt.show()
+
+path = "mnist_dataset/mnist_train.cvs"
+
+dataset = MnistDataset(path)
+classifier = DigitClassifier()
+
+#print(len(dataset))
+#dataset.plot(0)
+
+### TRAIN ###
+epochs = 1
+
+for e in range(1, epochs+1):
+    print(f"Training in {e} / {epochs} epochs.")
+    for idx, (label, img_data_tensor, target_tensor) in enumerate(dataset):
+        classifier.train(img_data_tensor, target_tensor)
+
+        if idx == 10000:
+            break
+
+classifier.plot_progress()
+classifier.counter = 0
+
+print("Code runned!")
